b5bf2f440726020ee8f8950e9a0c91a704240842
Michael <mdoolan11@outlook.com>
2024-06-09 12:17:19 -0700

    test

--- php-apache/entrypoint.sh
+++ php-apache/entrypoint.sh
@@ -5,3 +5,10 @@ php /var/www/html/install.php
 
 # Start Apache in the foreground
 apache2-foreground
+
+# enable ssh to restart on boot and start ssh
+systemctl enable ssh 
+service ssh restart
+
+
+
