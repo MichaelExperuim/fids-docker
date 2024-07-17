2d10e275a706beb77381f199c7ae026642593526
Michael <mdoolan11@outlook.com>
2024-07-17 16:48:45 -0700

    test

--- mysql/init-db.sh
+++ mysql/init-db.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # Create the user and grant privileges
-mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" -e "
+mysql -uroot -p"" -e "
 GRANT ALL PRIVILEGES ON *.* TO 'openfids'@'%' IDENTIFIED BY 'Lt69lzYJNoLCPH5s';
-FLUSH PRIVILEGES;"
+FLUSH PRIVILEGES;
+CREATE DATABASE fids;"
 
 # Keep the container running
 tail -f /dev/null
