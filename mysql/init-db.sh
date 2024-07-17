600c52ee836cf40fac410e318f4d93056c5e7563
Michael <mdoolan11@outlook.com>
2024-07-17 15:05:09 -0700

    testing mysql copy command

--- mysql/init-db.sh
+++ mysql/init-db.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-# Wait for the MySQL server to start
-echo "Waiting for MySQL server to start..."
-while ! mysqladmin ping -h"localhost" --silent; do
-    sleep 1
-done
-
-OPENFIDS_IP=$(getent hosts openfids | awk '{ print $1 }')
-echo "$OPENFIDS_IP openfids" >> /etc/hosts
-
 # Create the user and grant privileges
 mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" -e "
 GRANT ALL PRIVILEGES ON *.* TO 'openfids'@'%' IDENTIFIED BY 'Lt69lzYJNoLCPH5s';
 FLUSH PRIVILEGES;"
+
+# Keep the container running
+tail -f /dev/null
+
+
